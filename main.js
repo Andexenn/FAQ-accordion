e43837d1c4c840a0dfdd66cc8829ea66c81ce946
ryan <phanhuynhhung0707@gmail.com>
2025-05-25 17:33:17 +0700

    fix CSS and done Javascript

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-const answers = document.querySelectorAll(".answer");
-const btns = document.querySelectorAll(".plus");
+//! the first solution, complex and not convenient for user
 
-for (let i = 0; i < btns.length; ++i) {
-  // console.log(btns[i]);
-  // console.log(answers[i]);
+// const answers = document.querySelectorAll(".answer");
+// const btns = document.querySelectorAll(".plus");
 
-  btns[i].addEventListener("click", (e) => {
-    if(answers[i].style.display === "none"){
-        answers[i].style.display = "block";
-        btns[i].setAttribute("src", "./assets/images/icon-minus.svg");
-        // e.stopImmediatePropagation();   
-    }
-    else{
-        answers[i].style.display = "none";
-        btns[i].setAttribute("src", "./assets/images/icon-plus.svg");
+// for (let i = 0; i < btns.length; ++i) {
+
+//   btns[i].addEventListener("click", (e) => {
+//     if(answers[i].style.display === "none"){
+//         answers[i].style.display = "block";
+//         btns[i].setAttribute("src", "./assets/images/icon-minus.svg");
+//     }
+//     else{
+//         answers[i].style.display = "none";
+//         btns[i].setAttribute("src", "./assets/images/icon-plus.svg");
+//     }
+//   });
+// }
+
+//! the second method, ez to understand and good for user
+
+const items = document.querySelectorAll(".item");
+
+items.forEach((item) => {
+  const p = item.querySelector("p");
+  const img = item.querySelector("img");
+  item.addEventListener("click", (e) => {
+    if (p.style.display === "none") {
+      p.style.display = "block";
+      img.setAttribute("src", "./assets/images/icon-minus.svg");
+    } else {
+      p.style.display = "none";
+      img.setAttribute("src", "./assets/images/icon-plus.svg");
     }
   });
-}
+});
